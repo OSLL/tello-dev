3f9af0b328b886fec37a178a5d2003196ce9d4b7
Evgeniy Lukin <vgn1lkn26@gmail.com>
2024-03-15 09:47:11 +0000

    createDockerNetwork method added

--- rtl_device/connect.py
+++ rtl_device/connect.py
@@ -1,8 +1,11 @@
 import json
 import wifi
 import subprocess
+import netifaces
+import docker
 from loguru import logger
-# Необходимо установить wireless-tools, ifupdown
+
+# Необходимо установить wireless-tools, network-manager
 
 
 def readNetworkConfiguration(json_path):
@@ -14,15 +17,18 @@ def connect(iface, ssid, password) -> bool:
     logger.info(f"Connecting device \'{iface}\' to \'{ssid}\'")
     scanner = wifi.Cell.all(iface)
     available_networks = [cell.ssid for cell in scanner]
+
+    output = ""
     if ssid not in available_networks:
         logger.error(f"SSID {ssid} unreacheble")
         return False
     try:
         output = subprocess.check_output(
             ['nmcli', 'device', 'wifi', 'connect', ssid, 'password', password, 'ifname', iface]).decode(encoding='utf-8').replace("\n", "")
-
         if "successfully" in str(output):
-            logger.success(f'Device \'{iface}\' successfully connected to \'{ssid}\'')
+            logger.info(netifaces.ifaddresses(iface))
+            logger.success(
+                f'Device \'{iface}\' successfully connected to \'{ssid}\'')
             return True
         else:
             logger.error(f"Error while connecting to {ssid}")
@@ -35,7 +41,30 @@ def connect(iface, ssid, password) -> bool:
     return False
 
 
+def createDockerNetwork(name, iface, gateway, subnet):
+    options = dict({("parent", iface)})
+    srv_pool = docker.types.IPAMPool(subnet=subnet, gateway=gateway)
+    ipam = docker.types.IPAMConfig(pool_configs=[srv_pool])
+    client = docker.from_env()
+    docker_networks = client.networks.list([name])
+    if docker_networks != []:
+        logger.warning(
+            f"Network with name {name} already exists, removing it...")
+        network = client.networks.get(docker_networks[0].id)
+        network.remove()
+        logger.info(f'Removed network with id {docker_networks[0].id}')
+    logger.info(f'Creating network with name {name}')
+    client.networks.create(name, "macvlan", options, ipam)
+
+
 if __name__ == "__main__":
-    ifaces = readNetworkConfiguration("networks.json")["ifaces"]
+    config = readNetworkConfiguration("networks.json")
+    ifaces = config["ifaces"]
+    docker_networks = config["docker_networks"]
+    
     for iface in ifaces:
         connect(iface, ifaces[iface]['ssid'], ifaces[iface]['password'])
+    for network in docker_networks:
+        createDockerNetwork(network, docker_networks[network]["iface"],
+                            docker_networks[network]["gateway"],
+                            docker_networks[network]["subnet"])
