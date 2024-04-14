83effa6e36a08ad264c9faef8fd74f9825c92ee1
TokGrin <90797638+TokGrin@users.noreply.github.com>
2024-04-14 08:01:28 +0300

    fixed main bug with following marker

--- swarm/scripts/solutions/following/config.py
+++ swarm/scripts/solutions/following/config.py
@@ -1,13 +1,13 @@
 import cv2
 from utils.load_camera_settings import load_camera_settings
 
-# Config for following solutions
-
 FRAME_RATE = 30
 FRAME_SIZE = (960, 720)
 MARKER_SIZE = 50  # in pixels
 MARKER_SIZE_RANGE = 15
-MARKER_NUMBER = 304
+MARKER_CENTER = 304
+MARKER_STOP = 303  # random
+MARKER_MOVE = 302  # random
 X_RANGE = 125
 Y_RANGE = 125
 TIMEOUT = 10
@@ -19,8 +19,8 @@ MARKER_LEN = 6.5
 
 camera_matrix, camera_distortion = load_camera_settings()
 
-DISTANCE = 125  # in cm
+DISTANCE = 60  # in cm
 DISTANCE_RANGE = 25
 
-ARUCO_DICT = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
+ARUCO_DICT = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_APRILTAG_36h11)
 ARUCO_PARAMS = cv2.aruco.DetectorParameters()
