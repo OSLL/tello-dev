d4962e117c4c22d7f08c72ee0946c1b899ab7d26
Evgeniy Lukin <vgn1lkn26@gmail.com>
2024-04-25 18:59:04 +0300

    added client application

---
+++ swarm/scripts/rpc/client.py
@@ -0,0 +1,32 @@
+import argparse
+import requests
+from jsonrpcclient import request
+
+
+def main():
+    """
+    CLI application to read a command from the user.
+    """
+    parser = argparse.ArgumentParser(
+        description='CLI application to read a command')
+    parser.add_argument('-c', '--command', type=str, help='Command to execute')
+    parser.add_argument('--ip', type=str, help='IP address')
+    args = parser.parse_args()
+
+    if args.command:
+        print(f'Executing command: {args.command}')
+        if args.ip:
+            print(f'IP Address: {args.ip}')
+            url = "http://" + {args.ip} + ":65001"
+
+            req = request(url, "exec", args.command)
+            response = requests.post(url, json=req)
+            print(f"RESULT for '{url}' : {response.json()}")
+        else:
+            print('No IP address provided.')
+    else:
+        print('You need to pass the --command argument with a command to execute.')
+
+
+if __name__ == "__main__":
+    main()
